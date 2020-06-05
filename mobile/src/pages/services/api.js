d9c6c0a83c00af9ceed95a5ce7c23dcfa21b47f2
Thiago Santana Batista <thiagosanbat@gmail.com>
2020-06-05 14:19:19 -0300

    Ajustes nas dependências

--- mobile/src/pages/services/api.js
+++ mobile/src/pages/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 /* baseURL: 'http://192.168.1.8:3333' */ 
 
 const api = axios.create({
-    baseURL: 'http://192.168.1.8:3333'
+    baseURL: 'http://localhost:3333'
 })
 
 export default api
